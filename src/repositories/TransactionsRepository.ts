56b61c26f36365e19781005efd3270530f659172
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2020-11-03 21:13:18 -0300

    Conclusão de desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,5 @@
+import { v4 as uuid } from 'uuid';
+
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -14,15 +16,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { transactions } = this;
+
+    const incomes = transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const outcomes = transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+
+    const incomesValues = incomes.map(income => income.value);
+    const outcomesValues = outcomes.map(outcome => outcome.value);
+
+    const income = incomesValues.reduce(reducer, 0);
+    const outcome = outcomesValues.reduce(reducer, 0);
+
+    const balance = { income, outcome, total: income - outcome };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const id = uuid();
+
+    const transaction = { id, title, value, type };
+
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new Error('Invalid balance for transaction');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
